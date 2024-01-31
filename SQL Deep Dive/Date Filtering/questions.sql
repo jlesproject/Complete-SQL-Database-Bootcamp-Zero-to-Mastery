a2171d65de99ad804d33204cda7b68c1b26ffff5
jlesproject <143852314+jlesproject@users.noreply.github.com>
2024-01-30 23:20:58 -0500

    Update questions.sql

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -2,7 +2,10 @@
 * DB: Employees
 * Table: employees
 * Question: Get me all the employees above 60, use the appropriate date functions
-*/
+*/SELECT *
+FROM employees
+WHERE AGE(current_date, birth_date) > INTERVAL '60 years';
+
 
 SELECT * FROM employees;
 
@@ -10,7 +13,11 @@ SELECT * FROM employees;
 * DB: Employees
 * Table: employees
 * Question: How many employees where hired in February?
-*/
+*/SELECT COUNT(*)
+FROM employees
+WHERE EXTRACT(MOnth FROM hire_date) = 2 
+
+24448
 
 SELECT * FROM employees;
 
@@ -18,7 +25,11 @@ SELECT * FROM employees;
 * DB: Employees
 * Table: employees
 * Question: How many employees were born in november?
-*/
+*/SELECT COUNT(*)
+FROM employees
+WHERE EXTRACT(MOnth FROM birth_date) = 11
+
+24500
 
 SELECT * FROM employees;
 
@@ -26,7 +37,7 @@ SELECT * FROM employees;
 * DB: Employees
 * Table: employees
 * Question: Who is the oldest employee? (Use the analytical function MAX)
-*/
+*/SELECT MAX(AGE(birth_date)) FROM employees;
 
 SELECT * FROM employees;
 
@@ -34,7 +45,12 @@ SELECT * FROM employees;
 * DB: Store
 * Table: orders
 * Question: How many orders were made in January 2004?
-*/
+*/SELECT COUNT(*)
+FROM orders
+WHERE EXTRACT(MOnth FROM orderdate) = 1 
+AND EXTRACT(YEAR FROM orderdate) = 2004
+
+1000
 
 SELECT * FROM orders;
 
